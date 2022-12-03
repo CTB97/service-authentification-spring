d9eef68e4ff106c59e640a74791cbd35a151cfa3
CTB97 <boyecheikhtidiane97@gmail.com>
2022-12-03 09:45:23 +0000

    init repo

---
+++ src/test/java/com/ctb/ServiceAuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
